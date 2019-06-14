b7371ab252cbc1edee12f3a2bdaf65af90f439bc
Your Name <matrix511997@gmail.com>
2019-06-14 15:15:15 +0200

    first commit

---
+++ app/src/main/java/com/example/menubar/qrattendance.java
@@ -0,0 +1,13 @@
+package com.example.menubar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class qrattendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrattendance);
+    }
+}
